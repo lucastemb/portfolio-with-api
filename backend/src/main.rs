c60b224ecefb43ea19902b2a224bbc0a7d8f14dc
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-05-16 14:59:43 -0400

    added get end points

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,12 @@
 
 //import actix_web to make an API
 use std::error::Error;
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use actix_web::{web, App, HttpServer, HttpResponse};
+use sqlx::Row;
 
 
 //project schema 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 struct Project {
     languages: Vec<String>,
     desc: String,
@@ -16,7 +17,7 @@ struct Project {
     link: String,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 struct WorkExp {
     company: String,
     resp: Vec<String>,
@@ -25,7 +26,7 @@ struct WorkExp {
     
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 struct Education {
     logo: String,
     school: String,
@@ -87,6 +88,70 @@ async fn add_education(education: web::Json<Education>, pool: web::Data<sqlx::Pg
     Ok(HttpResponse::Ok().body("Education added successfully"))
 }
 
+//GET request at /get-projects end point (get all projects from db)
+#[actix_web::get("/get-projects")]
+async fn get_projects(pool: web::Data<sqlx::PgPool>) -> Result<HttpResponse, Box<dyn Error>> {
+    let q = "SELECT * FROM project";
+    let query = sqlx::query(q);
+    
+    let rows = query.fetch_all(pool.get_ref()).await?;
+
+    let projects = rows.iter().map(|row| {
+        Project {
+            languages: row.get("languages"),
+            desc: row.get("descript"),
+            resp: row.get("responsibilities"),
+            creation: row.get("date_created"),
+            thumbnail: row.get("thumbnail"),
+            title: row.get("title"),
+            link: row.get("link"),
+        }
+    }).collect::<Vec<_>>();
+
+    Ok(HttpResponse::Ok().json(projects))
+}
+
+//GET request at /get-education end point (get all projects from db)
+#[actix_web::get("/get-education")]
+async fn get_education(pool: web::Data<sqlx::PgPool>) -> Result<HttpResponse, Box<dyn Error>> {
+    let q = "SELECT * FROM education";
+    let query = sqlx::query(q);
+    
+    let rows = query.fetch_all(pool.get_ref()).await?;
+
+    let education = rows.iter().map(|row| {
+        Education {
+            logo: row.get("logo"),
+            school: row.get("school"),
+            awards: row.get("awards"), 
+            years: row.get("years"),
+            ecs: row.get("ecs")
+        }
+    }).collect::<Vec<_>>();
+
+    Ok(HttpResponse::Ok().json(education))
+}
+
+//GET request at /get-projects end point (get all projects from db)
+#[actix_web::get("/get-workexp")]
+async fn get_workexp(pool: web::Data<sqlx::PgPool>) -> Result<HttpResponse, Box<dyn Error>> {
+    let q = "SELECT * FROM workexp";
+    let query = sqlx::query(q);
+    
+    let rows = query.fetch_all(pool.get_ref()).await?;
+
+    let workexp = rows.iter().map(|row| {
+        WorkExp {
+            company: row.get("company"),
+            resp: row.get("descript"),
+            dates: row.get("dates"),
+            logo: row.get("logo")
+        }
+    }).collect::<Vec<_>>();
+
+    Ok(HttpResponse::Ok().json(workexp))
+}
+
 //main function 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -102,7 +167,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let port = 8080; 
     println!("Starting server on port {}", port);
 
-    HttpServer::new(move || App::new().app_data(web::Data::new(pool.clone())).service(add_education).service(add_project).service(add_workxp))
+    HttpServer::new(move || App::new().app_data(web::Data::new(pool.clone())).service(add_education).service(add_project).service(add_workxp).service(get_projects).service(get_education).service(get_workexp))
         .bind(("127.0.0.1", port))?
         .workers(2)
         .run()
