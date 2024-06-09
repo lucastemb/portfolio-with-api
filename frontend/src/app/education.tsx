db227ecbf3f18ac3c64356b987b8ee1c4fdc5524
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-06-08 20:45:06 -0400

    feat: added titles and font to site

--- frontend/src/app/education.tsx
+++ frontend/src/app/education.tsx
@@ -13,12 +13,16 @@ const Education = ({educations}: EducationProps) => {
   return(
    <> 
    {educations ? ((educations.length > 0) ? (educations.map((education : Education, index : number)=> (
-        <div  className="border-2 p-3 m-3 rounded-md" key = {index}> 
+        <div  className="border-2 p-3 m-3 rounded-md flex flex-row justify-around" key = {index}> 
+          <div className="w-1/2 flex justify-center items-center">
+          <img className="w-1/2" src={education.logo}/>
+          </div> 
+          <div className="flex flex-col justify-center items-center">
           <p className="font-semibold text-xl"> {education.school} </p> 
-          <img src={education.logo}/>
           <p> {education.years} </p>
           <p> {education.ecs.join(', ')} </p>
           <p> {education.awards.join(', ')} </p>
+          </div>
         </div>
       ))) : "") : "Loading..."} 
     </>
